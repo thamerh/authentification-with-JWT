f2e6acfc413cb3feca6a4cbc5c139f1463525059
thamerh <hamdithamer99@gmail.com>
2022-09-08 22:52:37 +0100

    feat: import verfy token feature in express router and add information haw used

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import {Login,Register} from "../controllers/User.js"
+import { verifyToken } from "../middelware/VerifyToken.js";
 const router = express.Router();
 
 router.post('/Login', Login);
 router.post('/Register', Register);
+// if make private route verify token user before send response must call to verifyToken feature with the intial feature for this route
 export default router;
